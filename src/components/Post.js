1d39bc1b23c94c482eec51d0314fd6246bfd52e3
jiha <zhsclq5324@naver.com>
2024-02-25 18:36:32 +0900

    commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getPost } from "../api/firebase";
 import PostCard from "./PostCard";
 
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = React.memo(({ value, onChange, optionList }) => {
+  return (
+    <select
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[30%] md:w-[20%]"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+});
+
 export default function Post() {
+  const [sortType, setSortType] = useState("latest");
+
   const {
     isPending,
     data: posts,
@@ -13,6 +36,23 @@ export default function Post() {
     queryFn: getPost,
   });
 
+  // useEffect(() => {
+  //   console.log(posts);
+  // }, [posts]);
+
+  const getProcessedPostList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.postNumber) - parseInt(a.postNumber);
+      } else {
+        return parseInt(a.postNumber) - parseInt(b.postNumber);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(posts));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <>
       {isPending && (
@@ -21,8 +61,16 @@ export default function Post() {
         </p>
       )}
       {error && <p>{error}</p>}
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
       <ul className="flex flex-col items-center">
-        {posts && posts.map((post) => <PostCard key={post.id} post={post} />)}
+        {posts &&
+          getProcessedPostList().map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
       </ul>
     </>
   );
---
+++ src/hooks/useSort.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// export default function useSort(posts, sortType) {
+//   const copyList = [...posts];
+//   const getProcessedPostList = () => {
+//     const compare = (a, b) => {
+//       if (sortType === "latest") {
+//         return b.createdAt - a.createdAt;
+//       } else {
+//         return a.createdAt - b.createdAt;
+//       }
+//     };
+//     const sortedList = copyList.sort(compare);
+//     return sortedList;
+//   };
+//   return <div>useSort</div>;
+// }
